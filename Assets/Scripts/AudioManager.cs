2f6a2613e47ce07864f71bb2dd1bb264e322d801
AYDINALP <alp.aydnalp@gmail.com>
2022-12-24 21:12:00 +0300

    PONG FİNAL
    Game finished

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class AudioManager : MonoBehaviour
 {
     public Audio[] audios;
     public static bool isSoundOpen;
+    public GameObject Slider;
 
     private void Start()
     {
         isSoundOpen = true;
+
     }
 
 
@@ -31,11 +35,12 @@ public class AudioManager : MonoBehaviour
             }
         }
     }
+
     public void closesong()
     {
-        if (isSoundOpen)
+        if (Slider=0)
         {
-            foreach (var item in audios)
+            foreach(var item in audios)
             {
                 if (item.source != null)
                     item.source.volume = 0;
